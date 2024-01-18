71fe155559a9d5c39b207dcd49ad9674f90bc15a
Efimov <93506814+YAcanai@users.noreply.github.com>
2024-01-18 09:14:41 +0300

    framer

--- src/Components/Forms/AuthenticationForm.tsx
+++ src/Components/Forms/AuthenticationForm.tsx
@@ -7,6 +7,7 @@ import Input, { InputTypes } from 'Assets/Tempus-Ui/Components/Inputs/Input';
 import TextWithLine from 'Assets/Tempus-Ui/Components/Texts/Text-with-line';
 import AppRoutes from 'Utils/Routes/app-routes';
 import { validateAuthenticationForm } from 'Utils/ValidateData/validateAuthenticationForm';
+import { motion } from 'framer-motion';
 import { FC, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
@@ -31,68 +32,100 @@ const AuthenticationFrom: FC<AuthenticationFromProps> = ({
     const [Pass, setPass] = useState('');
     const [Name, setName] = useState('');
     const [Age, setAge] = useState('');
-
+    const container = {
+        hidden: { opacity: 1, scale: 0 },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            transition: {
+                delayChildren: 0.3,
+                staggerChildren: 0.2,
+            },
+        },
+    };
+    const item = {
+        hidden: { y: 10, opacity: 0 },
+        visible: {
+            y: 0,
+            opacity: 1,
+        },
+    };
     return (
         <>
-            <form
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    validateAuthenticationForm(
-                        Pass,
-                        Email,
-                        Name,
-                        Age,
-                        title,
-                        handlerSubmit,
-                    );
-                }}
-                className={FormsStyles.AuthForm}
-            >
-                <div className={FormsStyles.AuthForm__Title}>{title}</div>
-
-                <div className={FormsStyles.AuthForm__InputWrapper}>
-                    <Input
-                        Placeholder="Ваша почта"
-                        Change={(e) => setEmail(e.target.value)}
-                        Value={Email}
-                        Type={InputTypes.email}
-                    ></Input>
-                    <Input
-                        Placeholder="Ваш пароль"
-                        Change={(e) => setPass(e.target.value)}
-                        Value={Pass}
-                        Type={InputTypes.password}
-                    ></Input>
-                    {Path === AppRoutes.REGISTER && (
-                        <div className={FormsStyles.moreInputs}>
-                            <Input
-                                Placeholder="Имя"
-                                Change={(e) => setName(e.target.value)}
-                                Value={Name}
-                                Type={InputTypes.text}
-                                MaxLength={15}
-                            ></Input>
-                            <Input
-                                Placeholder="Возраст"
-                                Change={(e) => setAge(e.target.value)}
-                                Value={Age}
-                                DefaultValue="03"
-                                Type={InputTypes.number}
-                            ></Input>
+            <motion.ul variants={container} initial="hidden" animate="visible">
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        validateAuthenticationForm(
+                            Pass,
+                            Email,
+                            Name,
+                            Age,
+                            title,
+                            handlerSubmit,
+                        );
+                    }}
+                    className={FormsStyles.AuthForm}
+                >
+                    <motion.li variants={item}>
+                        <div className={FormsStyles.AuthForm__Title}>
+                            {title}
                         </div>
-                    )}
-                </div>
+                    </motion.li>
 
-                <Button
-                    Title={
-                        Path === AppRoutes.REGISTER
-                            ? 'Зарегестрироваться'
-                            : 'Войти'
-                    }
-                    Click={() => {}}
-                    Type={ButtonTypes.active}
-                ></Button>
-            </form>
+                    <div className={FormsStyles.AuthForm__InputWrapper}>
+                        <motion.li variants={item}>
+                            <Input
+                                Placeholder="Ваша почта"
+                                Change={(e) => setEmail(e.target.value)}
+                                Value={Email}
+                                Type={InputTypes.email}
+                            ></Input>
+                        </motion.li>
+                        <motion.li variants={item}>
+                            <Input
+                                Placeholder="Ваш пароль"
+                                Change={(e) => setPass(e.target.value)}
+                                Value={Pass}
+                                Type={InputTypes.password}
+                            ></Input>
+                        </motion.li>
+                        {Path === AppRoutes.REGISTER && (
+                            <motion.li variants={item}>
+                                <div className={FormsStyles.moreInputs}>
+                                    <Input
+                                        Placeholder="Имя"
+                                        Change={(e) => setName(e.target.value)}
+                                        Value={Name}
+                                        Type={InputTypes.text}
+                                        MaxLength={15}
+                                    ></Input>
+
+                                    <Input
+                                        Placeholder="Возраст"
+                                        Change={(e) => setAge(e.target.value)}
+                                        Value={Age}
+                                        DefaultValue="03"
+                                        Type={InputTypes.number}
+                                    ></Input>
+                                </div>
+                            </motion.li>
+                        )}
+                    </div>
+                    <motion.li variants={item}>
+                        <Button
+                            Title={
+                                Path === AppRoutes.REGISTER
+                                    ? 'Зарегестрироваться'
+                                    : 'Войти'
+                            }
+                            Click={() => {}}
+                            Type={ButtonTypes.active}
+                        ></Button>
+                    </motion.li>
+                </form>
+            </motion.ul>
+
             <div className={FormsStyles.BottomForm}>
                 <TextWithLine>Или</TextWithLine>
                 <AuthWithGoogle />
