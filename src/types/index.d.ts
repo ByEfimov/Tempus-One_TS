e70f37e52e2c8dc4f9066bf3382cfdca12ac074a
Efimov <93506814+YAcanai@users.noreply.github.com>
2024-01-20 13:27:52 +0300

    Revert "Delete src/types directory"
    This reverts commit 9e589238499e1774a2332c66fa245e6e7ca53ef0.

---
+++ src/types/index.d.ts
@@ -0,0 +1,12 @@
+declare module '*.module.css' {
+    const content: Record<string, string>;
+    export default content;
+}
+declare module '*.module.scss' {
+    const content: Record<string, string>;
+    export default content;
+}
+declare module 'feather-icons-react';
+declare module 'uuid';
+
+declare module '*.svg';
