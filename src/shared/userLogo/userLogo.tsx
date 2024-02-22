40842087f9384e4d2350df2f952087dbe65b343b
Efimov <nikitacanai@yandex.ru>
2024-02-22 13:09:43 +0300

    fix

--- src/shared/userLogo/userLogo.tsx
+++ src/shared/userLogo/userLogo.tsx
@@ -1,11 +1,15 @@
-import FakeLogo from '@/Assets/Icons/Header/user.svg';
+import FakeLogo from '@/app/assets/Icons/Header/user.svg';
 import { useAuth } from '@/app/hooks/useAuth';
 
 const UserLogo = ({ Logo }: { Logo?: string | null }) => {
   const { UserPhoto, UserIsAuth } = useAuth();
   return (
     <img
-      style={UserIsAuth || Logo ? { width: '100%', height: '100%', borderRadius: '50%' } : undefined}
+      style={
+        UserIsAuth || Logo
+          ? { width: '100%', height: '100%', borderRadius: '50%' }
+          : !UserIsAuth && !Logo && { padding: '15px' }
+      }
       src={(Logo && Logo) || (UserIsAuth && UserPhoto) || FakeLogo}
       alt=""
     />
