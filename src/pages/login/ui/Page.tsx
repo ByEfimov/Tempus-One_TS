230f0d21326a0b9dd8388898f9da1624377d7861
Efimov <nikitacanai@yandex.ru>
2024-03-15 16:38:36 +0300

    Back to TEM-79

--- src/pages/login/ui/Page.tsx
+++ src/pages/login/ui/Page.tsx
@@ -2,9 +2,7 @@ import loginUser from '../api/loginUser';
 import { useAppDispatch } from '@/app/hooks/redux-hooks';
 import { useAuth } from '@/app/hooks/useAuth';
 import AuthenticationFrom from '@/entities/authenticationForm/authenticationForm';
-import { setCookieOnAnotherDomain } from '@/features/cookie/setCookieOnAnotherDomain';
 import AppRoutes from '@/shared/routes/app-routes';
-import { useEffect } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 
 export function LoginPage() {
@@ -12,28 +10,6 @@ export function LoginPage() {
 
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-
-  function getCookie(cookieName: string) {
-    const name = cookieName + '=';
-    const decodedCookies = decodeURIComponent(document.cookie);
-    const cookieArray = decodedCookies.split(';');
-
-    for (let i = 0; i < cookieArray.length; i++) {
-      const cookie = cookieArray[i].trim();
-      if (cookie.indexOf(name) == 0) {
-        return cookie.substring(name.length, cookie.length);
-      }
-    }
-
-    return '';
-  }
-
-  // Вызов функции для чтения куки
-
-  useEffect(() => {
-    setCookieOnAnotherDomain('yourCookieName', 'yourCookieValue', 'https://tempus-one-ts.vercel.app');
-    console.log('Значение куки на другом сайте: ' + getCookie('yourCookieName'));
-  }, []);
 
   return !user.isAuth ? (
     <AuthenticationFrom
