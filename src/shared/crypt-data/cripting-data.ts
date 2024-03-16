4ba006b6e4dadfd476260c117e8e6f2a4940b585
Efimov <nikitacanai@yandex.ru>
2024-03-16 16:40:22 +0300

    TEM-85

--- src/shared/crypt-data/cripting-data.ts
+++ src/shared/crypt-data/cripting-data.ts
@@ -3,18 +3,20 @@ import CryptoJS from 'crypto-js';
 const myKey = import.meta.env.VITE_TEMPUS_CRIPTO_KEY;
 
 export function encryptData(data: string | undefined | null) {
-    if (data) {
-        return CryptoJS.AES.encrypt(data, myKey).toString();
-    } else {
-        return '';
-    }
+  if (data) {
+    const encrypted = CryptoJS.AES.encrypt(data, myKey).toString();
+    return btoa(encrypted);
+  } else {
+    return '';
+  }
 }
 
 export function decryptData(encryptedData: string | undefined | null) {
-    if (encryptedData) {
-        const bytes = CryptoJS.AES.decrypt(encryptedData, myKey);
-        return bytes.toString(CryptoJS.enc.Utf8);
-    } else {
-        return '';
-    }
+  if (encryptedData) {
+    const decoded = atob(encryptedData);
+    const bytes = CryptoJS.AES.decrypt(decoded, myKey);
+    return bytes.toString(CryptoJS.enc.Utf8);
+  } else {
+    return '';
+  }
 }
